7ede05aa7d83c718ea2fca06b175d83947a00408
Hugo PREVOST <DipStax.hugo@gmail.com>
2023-11-30 22:58:27 +0100

    feat(Core): implementation of network pipeline in the core

--- include/Core/Core.hpp
+++ include/Core/Core.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Pipeline/Market.hpp"
+#include "Pipeline/Network.hpp"
 
 class Core
 {
@@ -18,10 +19,13 @@ class Core
     private:
         bool m_running = false;
 
+        std::vector<std::shared_ptr<net::tcp::Socket>> m_client;
         OrderBook m_ob;
 
+        NetToSerial m_nt_to_sr;
         SerialToMarket m_sr_to_mk;
         MarketToNet m_mk_to_nt;
 
+        pip::Network<net::tcp::Socket> m_network;
         pip::Market m_market;
 };
