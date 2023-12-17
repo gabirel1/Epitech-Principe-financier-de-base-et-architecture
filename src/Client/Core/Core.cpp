68e59828b5de8d35c6e62f1143c85a62189349fe
DipStax <dipstax.hugo@gmail.com>
2023-12-17 21:21:15 +0100

    feat(Client): first core

---
+++ src/Client/Core/Core.cpp
@@ -0,0 +1,21 @@
+#include "Client/Core/Core.hpp"
+
+Core::Core(const net::Ip &_ip, uint32_t _tcp, uint32_t _udp)
+{
+    m_tcp.connect(_ip, _tcp);
+}
+
+Core::~Core()
+{
+    stop();
+}
+
+void Core::start()
+{
+    while (m_running) {}
+}
+
+void Core::stop()
+{
+    m_running = false;
+}
