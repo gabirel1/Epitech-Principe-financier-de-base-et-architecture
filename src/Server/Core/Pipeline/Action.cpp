6a64fc1d52b56e95b3d2b9364f8ba7eeb92d110c
gabriel knies <gabriel.knies@epitech.eu>
2023-12-21 23:32:14 +0100

    [feature/action_impl]: adding some documentation on what each action should do

--- src/Server/Core/Pipeline/Action.cpp
+++ src/Server/Core/Pipeline/Action.cpp
@@ -48,29 +48,29 @@ namespace pip
                 switch (msg_type)
                 {
                 case 'A':
-                    // Received --> Logon (35=A)
+                    // Received --> Logon (35=A) --> Login (user can now send orders)
                     // Reply --> Logon (35=A)
                     break;
                 case '0':
-                    // Received --> Heartbeat (35=0)
+                    // Received --> Heartbeat (35=0) --> Heartbeat (user is still connected)
                     // Reply --> Heartbeat (35=0)
                     break;
                 case 'D':
-                    // Received --> New Order Single (35=D)
+                    // Received --> New Order Single (35=D) --> New Order Single (user sent an order)
                     // Reply --> Execution Report (35=8)
                     break;
                 case 'F':
-                    // Received --> Order Cancel Request (35=F)
+                    // Received --> Order Cancel Request (35=F) --> Order Cancel Request (user sent an order cancel, the order should be fully canceled)
                     // Reply --> Execution Report (35=8) or Order Cancel Reject (35=9)
                     break;
                 case 'G':
-                    // Received --> Order Cancel/Replace Request (35=G)
+                    // Received --> Order Cancel/Replace Request (35=G) -> Order Cancel/Replace Request (user sent an order cancel, the order should be partially canceled, [meaning that the order quantity should be updated or the price should be changed])
                     // Reply --> Execution Report (35=8)
                     break;
                 case '5':
-                    // Received --> Logout
+                    // Received --> Logout --> Logout (user is now disconnected)
                     // Reply --> Logout ?? Not sure
-                case 'V': // Maybe not here (should be used in UDP server)
+                case 'V': // Maybe not here (should be used in UDP server) --> Ask for market data
                     // Received --> Market Data Request (35=V)
                     // Reply --> Market Data Request Reject (35=Y) or Market Data Snapshot/Full Refresh (35=W)
                     break;
