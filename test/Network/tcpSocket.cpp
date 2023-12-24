10a907dfb35a5bf09f64ddf7f8569fa2dba68ebb
DipStax <dipstax.hugo@gmail.com>
2023-12-24 16:48:43 +0100

    test(Socket): fix test port to use randomes one
    add one more test on send/receive

--- test/Network/tcpSocket.cpp
+++ test/Network/tcpSocket.cpp
@@ -7,12 +7,12 @@
 
 #define TEST_IP_TCP "127.0.0.1"
 
-class tcp_Socket_ConnectClose : public testing::Test
+class Socket_ConnectClose : public testing::Test
 {
     protected:
         void SetUp() override
         {
-            Logger::SetThreadName(THIS_THREAD_ID, "test::tcp_Socket_ConnectClose");
+            Logger::SetThreadName(THIS_THREAD_ID, "test::Socket_ConnectClose");
             acceptor.listen(0);
             port = acceptor.getPort();
         }
@@ -26,7 +26,7 @@ class tcp_Socket_ConnectClose : public testing::Test
         uint32_t port = 0;
 };
 
-TEST_F(tcp_Socket_ConnectClose, connect_close_server) {
+TEST_F(Socket_ConnectClose, connect_close_server) {
     net::tcp::Socket socket;
 
     ASSERT_TRUE(socket.connect(TEST_IP_TCP, port));
@@ -39,7 +39,7 @@ TEST_F(tcp_Socket_ConnectClose, connect_close_server) {
     EXPECT_FALSE(socket.is_open());
 }
 
-TEST_F(tcp_Socket_ConnectClose, connect_close_client) {
+TEST_F(Socket_ConnectClose, connect_close_client) {
     net::tcp::Socket socket;
 
     ASSERT_TRUE(socket.connect(TEST_IP_TCP, port));
@@ -53,12 +53,12 @@ TEST_F(tcp_Socket_ConnectClose, connect_close_client) {
     ASSERT_FALSE(client->is_open());
 }
 
-class tcp_Socket_SendRecv : public testing::Test
+class Socket_SendRecv : public testing::Test
 {
     protected:
         void SetUp() override
         {
-            Logger::SetThreadName(THIS_THREAD_ID, "test::tcp_Socket_SendRecv");
+            Logger::SetThreadName(THIS_THREAD_ID, "test::Socket_SendRecv");
             acceptor.listen(0);
             socket.connect(TEST_IP_TCP, acceptor.getPort());
             client = acceptor.accept();
@@ -76,22 +76,20 @@ class tcp_Socket_SendRecv : public testing::Test
         net::Acceptor<net::tcp::Socket>::Client client;
 };
 
-TEST_F(tcp_Socket_SendRecv, single_send)
+TEST_F(Socket_SendRecv, single_send_single_recv)
 {
     const std::string send1 = "this is a test";
     int error = 0;
 
-    std::cout << "send" << std::endl;
     ASSERT_EQ(socket.send(send1), send1.size());
 
-    std::cout << "receive" << std::endl;
-    std::string recv = client->receive(send1.size() + 1, error);
-
-    ASSERT_EQ(error, 0);
-    ASSERT_EQ(recv, "this is a test");
+    std::string recv = client->receive(send1.size(), error);
+    ASSERT_EQ(error, send1.size());
+    ASSERT_EQ(recv.size(), send1.size());
+    ASSERT_EQ(recv, send1);
 }
 
-TEST_F(tcp_Socket_SendRecv, multiple_send_single_recv)
+TEST_F(Socket_SendRecv, multi_send_single_recv)
 {
     const std::string send1 = "this is ";
     const std::string send2 = "a test";
@@ -100,8 +98,28 @@ TEST_F(tcp_Socket_SendRecv, multiple_send_single_recv)
     ASSERT_EQ(socket.send(send1), send1.size());
     ASSERT_EQ(socket.send(send2), send2.size());
 
-    std::string recv = client->receive(send1.size() + send2.size() + 1, error);
-
-    ASSERT_EQ(error, 0);
+    std::string recv = client->receive(send1.size() + send2.size(), error);
+    ASSERT_EQ(error, send1.size() + send2.size());
+    ASSERT_EQ(recv.size(), send1.size() + send2.size());
     ASSERT_EQ(recv, send1 + send2);
+}
+
+TEST_F(Socket_SendRecv, single_send_multi_recv)
+{
+    int error = 0;
+    const std::string send1 = "this is a test";
+
+    static_assert(send1.size() % 2 == 0, "The sample data to send should have an odd length");
+
+    ASSERT_EQ(socket.send(send1), send1.size());
+
+    std::string recv = client->receive(send1.size() / 2, error);
+    ASSERT_EQ(error, send1.size() / 2);
+    ASSERT_EQ(recv.size(), send1.size() / 2);
+    ASSERT_EQ(recv, send1.substr(0, send1.size() / 2));
+
+    recv = client->receive(send1.size() / 2, error);
+    ASSERT_EQ(error, send1.size() / 2);
+    ASSERT_EQ(recv.size(), send1.size() / 2);
+    ASSERT_EQ(recv, send1.substr(send1.size() / 2, send1.size()));
 }
