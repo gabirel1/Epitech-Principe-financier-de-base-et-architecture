68f99094ed52064a145675e28af991161933396a
DipStax <dipstax.hugo@gmail.com>
2024-01-28 19:10:09 +0100

    fix: compilation error

--- src/Client/GUI/ThreadSocket.cpp
+++ src/Client/GUI/ThreadSocket.cpp
@@ -21,7 +21,7 @@ void ThreadSocket::run()
     while (m_running) {
 
 
-        std::cout << m_udp.receiveUDP(sizeof(udp::UDPPackage), error) <<std::endl;
+        std::cout << m_udp.receiveUDP(sizeof(data::UDPPackage), error) <<std::endl;
         // UDPPackage* udp = const_cast<UDPPackage*>(reinterpret_cast<const UDPPackage*>(&pack));
         // std::cout << udp->id << " || "<< udp->price << " || "<< udp->quantity << " || "<< udp->time << std::endl;
     }
